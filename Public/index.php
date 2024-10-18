38ecfbc548b6697124fde0843e6ef9b8fac86eac
Capitan Cloud <my25mb@aol.com>
2024-10-19 00:11:32 +0200

    Update index.php

--- Public/index.php
+++ Public/index.php
@@ -138,7 +138,7 @@ switch ($url) {
     }  
        
     if (filesize($picPath) <= APP_FILE_MAX_SIZE) { 
-      if ($fileExt = "jpg") {
+      if ($fileExt === "jpg") {
         header("Content-Type: image/jpeg");
       } else {
         header("Content-Type: image/" . $fileExt);
@@ -173,7 +173,7 @@ switch ($url) {
     $filePath = $JAR_PATH . DIRECTORY_SEPARATOR . $fileName;
        
     if (filesize($filePath) <= APP_FILE_MAX_SIZE) { 
-      if ($fileExt = "jpg") {
+      if ($fileExt === "jpg") {
         header("Content-Type: image/jpeg");
       } else {
         header("Content-Type: image/" . $fileExt);
